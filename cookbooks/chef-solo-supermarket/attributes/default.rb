6115db84c2ce2c7347bfb3b2762b44ffda8315ca
atishukla <atishayshukla@gmail.com>
2017-01-28 22:26:45 +0530

    basically going to default for jenkins with unlock screen

--- cookbooks/chef-solo-supermarket/attributes/default.rb
+++ cookbooks/chef-solo-supermarket/attributes/default.rb
@@ -6,8 +6,6 @@ default['java']['oracle']['accept_oracle_download_terms'] = true
 
 # attribute for jenkins installation
 
-# default['jenkins']['master']['jvm_options'] = '-Djenkins.install.runSetupWizard=false -Dhudson.model.User.allowNonExistentUserToLogin=true'
-default['jenkins-server']['admin']['username'] = "admin" 
-default['jenkins-server']['admin']['password'] = "admin" 
+
 
 
