0bd6aa4e750e25eba3607cf9e3e3fb91b8b8fd2f
atishukla <atishayshukla@gmail.com>
2017-01-28 14:38:21 +0530

    removed password creation from jenkins

--- cookbooks/chef-solo-supermarket/recipes/jenkins_install.rb
+++ cookbooks/chef-solo-supermarket/recipes/jenkins_install.rb
@@ -1,8 +1,3 @@
 # Including jenkins installation recipe from supermarket
 include_recipe 'jenkins::master'
 
-# Create password credentials
-jenkins_password_credentials 'ashukla' do
- description 'Atishay Shukla'
- password 'atishay'
-end
